a1c6fd228b0d2328506d2dfa9de8cf96f57070d0
Ben Bjurstrom <ben@jelled.com>
2017-03-06 07:59:12 -0800

    Add versioning to the api routes

--- routes.php
+++ routes.php
@@ -4,9 +4,9 @@ Route::group([
     'middleware' => ['auth:api', 'didbot.xml-http-request', 'didbot.throttle:450,15'],
     'namespace' => 'Didbot\DidbotApi\Controllers'
 ], function () {
-
-    Route::resource('dids', 'DidController');
-    Route::resource('tags', 'TagController');
-    Route::resource('sources', 'SourceController');
+    $prefix = env('ROUTE_PREFIX', '/api/1.0');
+    Route::resource($prefix . '/dids', 'DidController');
+    Route::resource($prefix . '/tags', 'TagController');
+    Route::resource($prefix . '/sources', 'SourceController');
 
 });
